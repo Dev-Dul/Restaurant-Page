c9dc8a940b5d84f871324da0e3c32fee3a48af67
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2024-11-08 18:50:03 +0100

    reviews page 90% done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-.
+const h2 = document.querySelector(".heading > h2");
+const revs = document.querySelectorAll(".rev");
+
+
+
+window.addEventListener("load", () => {
+    h2.classList.add("active");
+    revs.forEach((rev) => {
+      rev.classList.add("active");
+    }); 
+})
