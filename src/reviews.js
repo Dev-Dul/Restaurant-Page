4212bcdb4b2ac6e8dfccdf0ffce0568f75ee747a
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2024-11-08 19:21:30 +0100

    reviews page 95% done

---
+++ src/reviews.js
@@ -0,0 +1,51 @@
+const content = document.getElementById("content");
+
+const divCl = ["cust", "footer"];
+for(let i = 0; i < 2; i++){
+    let div = document.createElement("div");
+    div.setAttribute("class", divCl[i]);
+
+    content.appendChild(div);
+}
+
+
+// First Div (Cust)
+const hArr = ['heading', 'reviews'];
+for(let i = 0; i < 2; i++){
+    let div = document.createElement("div");
+    div.setAttribute("class", hArr[i]);
+
+    if(i === 0){
+        let h2 = document.createElement("h2");
+        let span = document.createElement("span");
+        let br = document.createElement("br");
+        span.appendChild(br);
+        span.textContent = "love Us!";
+
+        h2.textContent = "Here's Why Our Customers";
+        h2.appendChild(span);
+        div.appendChild(h2);
+
+    }else if(i === 1){
+        const revArr = ['Rayhaan', 'John Doe', 'Jane Doe', 'Abdul', 'Aisha', 'Muhammad', 'Hauwa', 'Al-amin', 'Umm', 'Lorem, Ipsum'];
+        for(let j = 0; j < 10; j++){
+            let div2 = document.createElement("div");
+            let h3 = document.createElement("h3");
+            let p = document.createElement("p");
+            
+            div2.setAttribute("class", "rev");
+            div2.setAttribute("style", `--r: ${j + 1}`);
+            h3.textContent = revArr[j];
+            p.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur aperiam blanditiis hic porro sit aspernatur, voluptatum est itaque? Iste ut enim hic consequatur aliquam. Doloribus quod quos cupiditate ipsam ea.";
+
+            div2.appendChild(h3);
+            div2.appendChild(p);
+
+            div.appendChild(div2);
+        }
+    }
+
+    content.children[0].appendChild(div);
+}
+
+// Second Div (Footer)
